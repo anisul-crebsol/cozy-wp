c66b45faacb9f63d5a66afa45c15f90b22252261
Anisul Kibria <anisul@crebsol.com>
2015-03-10 14:07:34 -0700

    Seperated homepage sections in includes directory

---
+++ includes/home-search.php
@@ -0,0 +1,25 @@
+		<section id="home-search-section" data-stellar-background-ratio="0.5">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-12" data-animation-direction="from-top" data-animation-delay="50">
+						
+						<h2 class="slider-title">Find the perfect home</h2>
+						<div class="slider-subtitle">With Cozy Real Estate HTML Template</div>
+					</div>
+					
+					<div id="home-search-buttons" class="col-sm-6 col-sm-offset-3" data-animation-direction="from-bottom" data-animation-delay="250">
+						<button class="btn btn-default active">For Sale</button>
+						<button class="btn btn-default">For Rent</button>
+						
+						<div class="input-group">
+							<input type="text" placeholder="City, State, Country, etc..." name="home_search" id="home_search" class="form-control" />
+							<span class="input-group-btn">
+								<button class="btn btn-default" type="button"><i class="fa fa-search"></i>Search</button>
+							</span>
+						</div>
+						
+						<a href="#" class="advanced-search">Advanced Search</a>
+					</div>
+				</div>
+			</div>
+		</section>
---
+++ includes/main-content.php
@@ -0,0 +1,123 @@
+					<div class="main col-sm-8">
+						<h1 class="section-title" data-animation-direction="from-bottom" data-animation-delay="50">Template Features</h1>
+
+						<div class="feature col-sm-4" data-animation-direction="from-bottom" data-animation-delay="250">
+							<i class="fa fa-pencil"></i>
+							<h3>Simple, Clean & Modern Design</h3>
+							<p>Pellentesque elementum libero enim, eget gravida nunc laoreet et. Nullam ac enim auctor, fringilla risus at, imperdiet turpis.</p>
+							<a href="about.html" class="btn btn-default-color">Read More</a>
+						</div>
+						<div class="feature col-sm-4" data-animation-direction="from-bottom" data-animation-delay="450">
+							<i class="fa fa-tablet"></i>
+							<h3>Fully Responsive & Retina Ready</h3>
+							<p>Pellentesque elementum libero enim, eget gravida nunc laoreet et. Nullam ac enim auctor, fringilla risus at, imperdiet turpis.</p>
+							<a href="about.html" class="btn btn-default-color">Read More</a>
+						</div>
+						<div class="feature col-sm-4" data-animation-direction="from-bottom" data-animation-delay="650">
+							<i class="fa fa-copy"></i>
+							<h3>+20 different Pages highly Customizable</h3>
+							<p>Pellentesque elementum libero enim, eget gravida nunc laoreet et. Nullam ac enim auctor, fringilla risus at, imperdiet turpis.</p>
+							<a href="about.html" class="btn btn-default-color">Read More</a>
+						</div>
+						
+						
+						<h1 class="section-title" data-animation-direction="from-bottom" data-animation-delay="50">Property Gallery</h1>
+						<p class="center" data-animation-direction="from-bottom" data-animation-delay="150">Pellentesque elementum libero enim, eget gravida nunc laoreet et. Nullam ac enim auctor, fringilla risus at, imperdiet turpis. Pellentesque elementum libero enim, eget gravida nunc laoreet et.</p>
+					
+						<div id="property-gallery" class="owl-carousel property-gallery">
+							<div class="item" data-animation-direction="from-bottom" data-animation-delay="350">
+								<a href="http://placehold.it/760x670" data-gal="prettyPhoto[gallery]" title="Lorem ipsum"><span class="btn btn-default">+</span></a>
+								<img src="http://placehold.it/246x217" alt="" />
+							</div>
+			
+							<div class="item" data-animation-direction="from-bottom" data-animation-delay="350">
+								<a href="http://placehold.it/760x670" data-gal="prettyPhoto[gallery]" title="Lorem ipsum"><span class="btn btn-default">+</span></a>
+								<img src="http://placehold.it/246x217" alt="" />
+							</div>
+							
+							<div class="item" data-animation-direction="from-bottom" data-animation-delay="350">
+								<a href="http://placehold.it/760x670" data-gal="prettyPhoto[gallery]" title="Lorem ipsum"><span class="btn btn-default">+</span></a>
+								<img src="http://placehold.it/246x217" alt="" />
+							</div>
+							<div class="item">
+								<a href="http://placehold.it/760x670" data-gal="prettyPhoto[gallery]" title="Lorem ipsum"><span class="btn btn-default">+</span></a>
+								<img src="http://placehold.it/246x217" alt="" />
+							</div>
+							<div class="item">
+								<a href="http://placehold.it/760x670" data-gal="prettyPhoto[gallery]" title="Lorem ipsum"><span class="btn btn-default">+</span></a>
+								<img src="http://placehold.it/246x217" alt="" />
+							</div>
+							<div class="item">
+								<a href="http://placehold.it/760x670" data-gal="prettyPhoto[gallery]" title="Lorem ipsum"><span class="btn btn-default">+</span></a>
+								<img src="http://placehold.it/246x217" alt="" />
+							</div>
+						</div>
+						
+						
+						<!-- BEGIN LATEST NEWS -->
+						<h1 class="section-title" data-animation-direction="from-bottom" data-animation-delay="50">Latest News</h1>
+						<div class="latest-news list-style clearfix">
+							<div class="item col-sm-12" data-animation-direction="from-bottom" data-animation-delay="250">
+								<div class="image">
+									<a href="blog-detail.html">
+										<span class="btn btn-default"><i class="fa fa-file-o"></i> Read More</span>
+									</a>
+									<img src="http://placehold.it/766x515" alt="" />
+								</div>
+								<div class="tag"><i class="fa fa-file-text"></i></div>
+								<div class="info-blog">
+									<ul class="top-info">
+										<li><i class="fa fa-calendar"></i> July 30, 2014</li>
+										<li><i class="fa fa-comments-o"></i> 2</li>
+										<li><i class="fa fa-tags"></i> Properties, Prices, best deals</li>
+									</ul>
+									<h3>
+										<a href="blog-detail.html">How to get your dream property for the best price?</a>
+									</h3>
+									<p>Sed rutrum urna id tellus euismod gravida. Praesent placerat, mauris ac pellentesque fringilla, tortor libero condimen.</p>
+								</div>
+							</div>
+							<div class="item col-sm-12" data-animation-direction="from-bottom" data-animation-delay="450">
+								<div class="image">
+									<a href="blog-detail.html">
+										<span class="btn btn-default"><i class="fa fa-file-o"></i> Read More</span>
+									</a>
+									<img src="http://placehold.it/766x515" alt="" />
+								</div>
+								<div class="tag"><i class="fa fa-film"></i></div>
+								<div class="info-blog">
+									<ul class="top-info">
+										<li><i class="fa fa-calendar"></i> July 24, 2014</li>
+										<li><i class="fa fa-comments-o"></i> 4</li>
+										<li><i class="fa fa-tags"></i> Tips, Mortgage</li>
+									</ul>
+									<h3>
+										<a href="blog-detail.html">7 tips to get the best mortgage.</a>
+									</h3>
+									<p>Sed rutrum urna id tellus euismod gravida. Praesent placerat, mauris ac pellentesque fringilla, tortor libero condimen.</p>
+								</div>
+							</div>
+							<div class="item col-sm-12" data-animation-direction="from-bottom" data-animation-delay="650">
+								<div class="image">
+									<a href="blog-detail.html">
+										<span class="btn btn-default"><i class="fa fa-file-o"></i> Read More</span>
+									</a>
+									<img src="http://placehold.it/766x515" alt="" />
+								</div>
+								<div class="tag"><i class="fa fa-file-text"></i></div>
+								<div class="info-blog">
+									<ul class="top-info">
+										<li><i class="fa fa-calendar"></i> July 05, 2014</li>
+										<li><i class="fa fa-comments-o"></i> 1</li>
+										<li><i class="fa fa-tags"></i> Location, Price, House</li>
+									</ul>
+									<h3>
+										<a href="blog-detail.html">House, location or price: What's the most important factor?</a>
+									</h3>
+									<p>Sed rutrum urna id tellus euismod gravida. Praesent placerat, mauris ac pellentesque fringilla, tortor libero condimen.</p>
+								</div>
+							</div>
+						</div>
+						<!-- END LATEST NEWS -->
+						
+					</div>
